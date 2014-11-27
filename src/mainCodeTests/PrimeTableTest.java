1017eb75593a6cb501680c787ccd8608b07dea4d
Grey_Ham <graham.brown137@gmail.com>
2014-11-27 11:10:24 -0800

    Tests running and working for simple cases.

--- src/mainCodeTests/PrimeTableTest.java
+++ src/mainCodeTests/PrimeTableTest.java
@@ -7,42 +7,69 @@ import java.util.ArrayList;
 import mainCode.PrimeTable;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import exceptions.BadLimitException;
 
 
 public class PrimeTableTest {
-	
-	PrimeTable primeTable;
-	ArrayList<Integer> table;
+
+	private PrimeTable primeTable;
+	private ArrayList<Integer> table;
+	private int limit = 0;
+
 	
 	@Before
 	public void primeTableInit(){
-		PrimeTable.setLimit(100);
-		primeTable = PrimeTable.getPrimeTable();
-		table = primeTable.getTable();
+		limit = 1;
+		try {
+			primeTable = PrimeTable.getPrimeTable(limit);
+			table = primeTable.getTable();
+		} catch (BadLimitException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getStackTrace());
+		}
+
 	}
-	
+
 	@Test
 	public void testSingleton() {
-		assertTrue(PrimeTable.getPrimeTable() == primeTable);
-		PrimeTable tempTable = PrimeTable.getPrimeTable();
-		assertTrue(primeTable == tempTable);
+		try {
+			assertTrue(PrimeTable.getPrimeTable(limit) == primeTable);
+			PrimeTable tempTable = PrimeTable.getPrimeTable(limit);
+			assertTrue(primeTable.equals(tempTable));
+		} catch (BadLimitException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getStackTrace());
+		}
+	}
+
+	@Test 
+	public void testZeroLimit() {
+			int checkVal = 0;
+			try {
+				primeTable.regenerate(0);
+				checkVal = 1;
+			} catch (BadLimitException e) {
+				assertEquals(checkVal, 0);
+			}
 	}
 	
-	@Test
-	public void testZeroCase() {
-		primeTable = null;
-		PrimeTable.setLimit(0);
-		
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testIndexOutOfBoundsException() {
+	    ArrayList emptyList = new ArrayList();
+	    Object o = emptyList.get(0);
 	}
-	
+
 	@Test
 	public void testValues() {
-		assertEquals(table.get(0), (Integer) 1);
+		assertEquals(table.get(0), (Integer) 0);
 	}
-	
+
 	@Test
 	public void testMultiCase() {
-		
+
 	}
 }
